a365b0c87b79f9d257b52ddf2b4474982025e15c
RandyCabrera08 <78192213+RandyCabrera08@users.noreply.github.com>
2024-08-08 22:11:01 -0400

    Add files via upload

---
+++ Chatbot/responses.py
@@ -0,0 +1,54 @@
+# responses.py
+
+questions_and_responses = [
+    {
+        "response": "Saludos, espero que se encuentre bien. ¿Qué carrera del ITLA desea estudiar?",
+        "recognized_words": ['hola', 'klk', 'saludos', 'buenas'],
+        "single_response": True
+    },
+    {
+        "response": "Estamos ubicados en La Caleta, Boca Chica.",
+        "recognized_words": ['ubicados', 'dirección', 'donde', 'ubicación'],
+        "single_response": True
+    },
+    {
+        "response": "El ITLA ofrece carreras como Software, Mecatrónica, Ciencia de Datos, Seguridad Informática, entre otras.",
+        "recognized_words": ['carreras', 'ofrecen', 'programas', 'estudios'],
+        "single_response": True
+    },
+    {
+        "response": "El ITLA tiene una duración de aproximadamente 2 años y medio en sus carreras técnicas.",
+        "recognized_words": ['duración', 'tiempo', 'años', 'estudios'],
+        "required_words": ['carrera']
+    },
+    {
+        "response": "El proceso de inscripción se realiza en línea a través de la página oficial del ITLA.",
+        "recognized_words": ['inscripción', 'inscribirse', 'registro', 'matrícula'],
+        "single_response": True
+    },
+    {
+        "response": "El costo por trimestre en el ITLA varía según la carrera, pero es accesible en comparación con otras instituciones.",
+        "recognized_words": ['costo', 'precio', 'pago', 'trimestre'],
+        "single_response": True
+    },
+    {
+        "response": "Las clases se ofrecen tanto de manera presencial como en modalidad virtual.",
+        "recognized_words": ['modalidad', 'clases', 'presencial', 'virtual'],
+        "single_response": True
+    },
+    {
+        "response": "Sí, el ITLA ofrece becas para estudiantes destacados.",
+        "recognized_words": ['becas', 'ayuda', 'financiamiento'],
+        "single_response": True
+    },
+    {
+        "response": "El ITLA realiza jornadas de admisión en diferentes fechas del año.",
+        "recognized_words": ['admisión', 'ingreso', 'proceso'],
+        "single_response": True
+    },
+    {
+        "response": "Sí, hay facilidades de transporte público para llegar al ITLA.",
+        "recognized_words": ['transporte', 'llegar', 'cómo', 'acceder'],
+        "single_response": True
+    }
+]
